144bf5356b1e591c2c9edaa5226b17e68df69def
Steve King <steve@dodgestar.com>
2013-03-16 09:29:40 +0000

    Use random port for tests

--- test/test-middleware.js
+++ test/test-middleware.js
@@ -2,8 +2,6 @@ var TestCase = require('unit-test').TestCase,
    Assertions = require('unit-test').Assertions,
    sinon = require('unit-test').Sinon,
    FileSystem = require('fs'),
-   Phantom = require('phantom'),
-   TaskRunner = require('task-runner').TaskRunner,
    Express = require('express'),
    GoogleAuth = require('../src/index'),
    Promise = require('promise-lite').Promise,
@@ -11,12 +9,12 @@ var TestCase = require('unit-test').TestCase,
 
 var persistenceFilePath = __dirname + '/.googleauth';
 
-var phantom, taskRunner;
 var express;
 var googleAuth;
 var middleware;
 var promise;
 var mainRoute;
+var testPort;
 
 module.exports = new TestCase('Middleware', {
 
@@ -38,40 +36,28 @@ module.exports = new TestCase('Middleware', {
       express.get('/', mainRoute = sinon.spy(function(req, res, next) {
          res.send({ query: req.query, params: req.params, url: req.url, user: req.session.user });
       }));
-      express.listen(8989);
+      express.server = express.listen(0, function() {
+         testPort = express.server.address().port;
+      });
+   },
+
+   tearDown: function () {
+      express.server.close();
 
       try {
          FileSystem.unlinkSync(persistenceFilePath);
       }
-      catch (e) {
-      }
-   },
-
-   tearDown: function () {
-      if (phantom) {
-         phantom.exit();
-      }
-      express.st
+      catch (e) {}
    },
 
    'test Can make a request to the server': function(done) {
-      setTimeout(function() {
-
-         promise.done(function () {
-            XHR('http://localhost:8989', {
-               success: function (data) {
-                  console.log('OK', data.toString('utf8'));
-               },
-               error: function () {
-                  console.log('error')
-               },
-               complete: function () {
-                  done();
-               }
-            });
-         })
-
-      }, 10000)
-
+      promise.done(function () {
+         XHR('http://localhost:' + testPort, {
+            complete: function (xhr) {
+               Assertions.assertEquals(302, xhr.statusCode, "Request without authentication data should be redirected");
+               done();
+            }
+         });
+      });
    }
 });
